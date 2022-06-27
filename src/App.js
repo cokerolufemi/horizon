7e77721d61d045fbc79c82ba06dcb7e134b5cc82
coker <goocng@gmail.com>
2022-06-27 05:46:48 -0700

    coker2

--- src/App.js
+++ src/App.js
@@ -3,23 +3,141 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+   <>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="https://i.pinimg.com/originals/1c/54/f7/1c54f7b06d7723c21afc5035bf88a5ef.png" alt="logo" class="get" />
         </a>
-      </header>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+  
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+             <li class="nav-item active">
+                <a class="nav-link" href="#"><span class="sr-only">(current)</span></a>
+             </li>
+       
+            </ul>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <img src="https://image.shutterstock.com/image-vector/ghana-national-flag-africa-coat-600w-1125806549.jpg" alt="Ghana" class="size" />
+                <img src="https://m.media-amazon.com/images/I/31ibIG9a4IL._AC_SX425_.jpg" alt=" Nigeria" class="sizes text" />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-TsnbAMjwdL6ssNwC_RwujGVIJF2EjL6VD7NqQorsfigvtD3gY5ONc1mpacs2LaMTp2w&usqp=CAU" alt="Kenya" class="siz" />
+            </div> 
+        </div>
+        <a href="about.html">About us</a>
+    </nav>
+    <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="1" class=""></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="2" class=""></li>
+              </ol>
+          <div class="carousel-inner  carousel-content">
+              <div class="carousel-item active">
+                  <img src="https://images.unsplash.com/photo-1600047509807-ba8f99d2cdde?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=692&amp;q=80" class="d-block w-100 h-55" alt="house" />
+              </div>
+   <div class="carousel-item">
+      <img src="https://images.unsplash.com/photo-1571055107559-3e67626fa8be?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1031&amp;q=80" class="d-block w-100 h-55" alt="house" />
     </div>
-  );
+    <div class="carousel-item">
+      <img src="https://images.unsplash.com/photo-1600585154526-990dced4db0d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGhvdXNlc3xlbnwwfHwwfHw%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="d-block w-100 h-55" alt="house" />
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+ </div>
+ </div>
+ </div>
+ <br />
+ <br />
+
+    <div class="row">
+    	<div class="col-md-12">
+    		<h3 class="text-center">PLANS</h3>
+    	</div>   	
+   </div>
+
+
+   <div class="row">
+    	<div class="col-md-3">
+    		<img src="https://images.unsplash.com/photo-1630650231815-a567e2ed26cc?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aG91c2VzfGVufDB8fDB8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="img-thumbnail tom" alt="house" />
+    		<p class="text-center">VIRTUAL OFFICES</p>
+    		<p class="text-center closemargin">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do ei
+    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+    	</p></div>
+
+   	    <div class="col-md-3">
+    		<img src="https://images.unsplash.com/photo-1600585154526-990dced4db0d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGhvdXNlc3xlbnwwfHwwfHw%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="img-thumbnail tom" alt="house" />
+    		<p class="text-center">CUSTOMISED OFFICES</p>
+    		<p class="text-center closemargin">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do e
+    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+
+    	</p></div>
+    	<div class="col-md-3">
+    		<img src="https://images.unsplash.com/photo-1602343168117-bb8ffe3e2e9f?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGhvdXNlc3xlbnwwfHwwfHw%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="img-thumbnail tom" alt="house" />
+    		<p class="text-center">SERVICED OFFICES</p>
+    		<p class="text-center closemargin">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+    	</p></div>
+        <div class="col-md-3">
+        	<img src="https://images.unsplash.com/photo-1600607687920-4e2a09cf159d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdXNlc3xlbnwwfHwwfHw%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="img-thumbnail tom" alt="https://images.unsplash.com/photo-1600607687920-4e2a09cf159d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdXNlc3xlbnwwfHwwfHw%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" />
+        	<p class="text-center">MEETING OFFICES</p>
+    		<p class="text-center closemargin">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+        </p></div>
+   </div>
+</div>
+
+
+
+<footer class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<h3 class="text-center uniform">VISIT US IN KENYA</h3>
+        <p class="text-center capital">Nairobi,Kenya</p>
+        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim v.
+      </p>
+			</div>
+			<div class="col-md-4">
+				<h3 class="text-center uniform">VISIT US IN GHANA</h3>
+        <p class="text-center capital">Accra,Ghana</p>
+        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,.
+      </p>
+			</div>
+			<div class="col-md-4">
+				<h3 class="text-center uniform">VISIT US IN NIGERIA</h3>
+        <p class="text-center capital">Lagos,Nigeria</p>
+        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+      </p>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+</>
+
+      );
 }
 
 export default App;
